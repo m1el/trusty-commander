e5572b6ddd1165d1132581cc320048e1f2b71210
Igor null <m1el.2027@gmail.com>
2017-04-18 01:15:09 +0300

    simple unsafe paint test

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,112 @@
 #![no_main]
 extern crate winapi;
-use winapi::{HINSTANCE, c_int, STD_OUTPUT_HANDLE};
+extern crate user32;
 
+use winapi::*;
 mod win_layer;
 use win_layer::*;
 
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "C"
+pub extern "system"
 fn wWinMain(
-    _instance: HINSTANCE,
+    instance: HINSTANCE,
     _prev_instance: HINSTANCE,
     _cmd_line: *const u16,
     _cmd_show: c_int)
     -> c_int
 {
-    match our_main() { Ok(_) => 0, Err(x) => x as c_int }
+    match our_main(instance) { Ok(_) => 0, Err(x) => x as c_int }
 }
 
 #[no_mangle]
-pub extern "C"
+pub extern "system"
 fn main(
     _argc: c_int,
     _argv: *const *const u8)
     -> c_int
 {
-    match our_main() { Ok(_) => 0, Err(x) => x as c_int }
+    let instance = GetModuleHandleW(None).unwrap_or(0 as HINSTANCE);
+    match our_main(instance) { Ok(_) => 0, Err(x) => x as c_int }
 }
 
-fn our_main() -> Result<u32, u32>
+extern "system"
+fn wnd_proc(
+    hwnd: HWND,
+    msg: UINT,
+    param: WPARAM,
+    para: LPARAM)
+    -> LRESULT
 {
+    //println!("winmsg: 0x{:04x}", msg);
+    match msg {
+        WM_CLOSE => match DestroyWindow(hwnd) {
+            Ok(_) => 0,
+            Err(x) => x as LRESULT
+        },
+        WM_PAINT => {
+            unsafe {
+                let mut ps = std::mem::zeroed::<PAINTSTRUCT>();
+                let hdc = user32::BeginPaint(hwnd, &mut ps as *mut PAINTSTRUCT);
+                let mut r = std::mem::zeroed::<RECT>();
+                user32::GetClientRect(hwnd, &mut r as *mut RECT);
+                user32::FillRect(hdc, &r, (winapi::COLOR_WINDOW + 2) as HBRUSH);
+                user32::EndPaint(hwnd, &ps);
+            }
+            0
+        }
+        WM_DESTROY => { PostQuitMessage(0); 0 },
+        _ => DefWindowProcW(hwnd, msg, param, para)
+    }
+}
+
+fn our_main(instance: HINSTANCE) -> Result<u32, u32>
+{
+    let wnd_cls = WNDCLASSEXW {
+        cbSize: std::mem::size_of::<WNDCLASSEXW>() as UINT,
+        style: CS_HREDRAW | CS_VREDRAW,
+        lpfnWndProc: Some(wnd_proc),
+        cbClsExtra: 0,
+        cbWndExtra: 0,
+        hInstance: instance,
+        hIcon: unsafe { user32::LoadIconW(0 as HINSTANCE, IDI_APPLICATION) },
+        hCursor: unsafe { user32::LoadCursorW(0 as HINSTANCE, IDC_ARROW) },
+        hbrBackground: (winapi::COLOR_WINDOW + 1) as HBRUSH,
+        lpszMenuName: 0 as *const u16,
+        lpszClassName: &[0x40u16, 0x40, 0x00] as *const u16,
+        hIconSm: unsafe { user32::LoadIconW(0 as HINSTANCE, IDI_APPLICATION) },
+    };
+
+    let atom = try!(RegisterClassExW(&wnd_cls));
+    let hwnd = try!(CreateWindowExW(
+        winapi::WS_EX_CLIENTEDGE,
+        WinClsW::Atom(atom),
+        Some(&[0x44, 0x44, 0x00]),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+        None, None, instance, None));
+
+    let _ = ShowWindow(hwnd, SW_SHOWDEFAULT);
+    let _ = try!(UpdateWindow(hwnd));
+
+    /*
+    'outer: loop {
+        for rmsg in peeking_msg_loop(None) {
+            let msg = try!(rmsg);
+            println!("msg, 0x{:04x}", msg.message);
+            if msg.message == WM_QUIT { break 'outer; }
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+    */
+
+    for rmsg in blocking_msg_loop(None) {
+        let msg = try!(rmsg);
+        //println!("msg, 0x{:04x}", msg.message);
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
     Ok(0)
 }
--- src/win_layer.rs
+++ src/win_layer.rs
@@ -3,10 +3,9 @@
 extern crate winapi;
 extern crate kernel32;
 extern crate user32;
+extern crate std;
 
-use winapi::{HANDLE, OVERLAPPED, INVALID_HANDLE_VALUE};
-use winapi::{HWND};
-use winapi::{c_int, c_uint};
+use winapi::*;
 
 #[allow(dead_code)]
 #[inline]
@@ -18,6 +17,54 @@ pub fn GetLastError() -> u32
 { unsafe { kernel32::GetLastError() } }
 
 #[inline]
+pub fn GetModuleHandleW(name: Option<&[u16]>) -> Result<HMODULE, u32>
+{
+    let handle = unsafe { kernel32::GetModuleHandleW(name.map_or(0 as LPCWSTR, |x|x.as_ptr() as LPCWSTR)) };
+
+    if handle != 0 as HMODULE { Ok(handle) }
+    else { Err(GetLastError()) }
+}
+
+#[inline]
+pub fn GetModuleHandleA(name: Option<&[u8]>) -> Result<HMODULE, u32>
+{
+    let handle = unsafe { kernel32::GetModuleHandleA(name.map_or(0 as LPCSTR, |x|x.as_ptr() as LPCSTR)) };
+    if handle != 0 as HMODULE { Ok(handle) }
+    else { Err(GetLastError()) }
+}
+
+#[inline]
+pub fn GetStdHandle(handle_id: u32) -> Result<Option<HANDLE>, u32>
+{
+    let handle = unsafe { kernel32::GetStdHandle(handle_id) };
+
+    if handle == 0 as HANDLE { Ok(None) }
+    else if handle != INVALID_HANDLE_VALUE { Ok(Some(handle)) }
+    else { Err(GetLastError()) }
+}
+
+#[inline]
+pub fn WriteFile(
+    handle: HANDLE,
+    buffer: &[u8],
+    overlapped: Option<&mut OVERLAPPED>)
+    -> Result<u32, u32>
+{
+    //assert!(buffer.len() <= u32::max_value() as usize, "buffer length has to fit into u32");
+    let buffer_len = buffer.len() as u32;
+    let mut written: u32 = 0u32;
+    let result = unsafe {
+        let buffer_ptr = buffer.as_ptr() as *mut winapi::c_void;
+        let written_ptr = &mut written as *mut u32;
+        let overlapped_ptr = overlapped.map_or(0 as *mut OVERLAPPED, |x|x as *mut OVERLAPPED);
+        kernel32::WriteFile(handle, buffer_ptr, buffer_len, written_ptr, overlapped_ptr)
+    };
+
+    if result != 0 { Ok(written) }
+    else { Err(GetLastError()) }
+}
+
+#[inline]
 pub fn GetConsoleWindow() -> Option<HWND>
 {
     let handle = unsafe { kernel32::GetConsoleWindow() };
@@ -27,9 +74,15 @@ pub fn GetConsoleWindow() -> Option<HWND>
 }
 
 #[inline]
-pub fn ShowWindow(hWnd: HWND, nCmdShow: c_int) -> Result<(), u32>
+pub fn ShowWindow(hWnd: HWND, nCmdShow: c_int) -> bool
 {
-    let result = unsafe { user32::ShowWindow(hWnd, nCmdShow) };
+    unsafe { user32::ShowWindow(hWnd, nCmdShow) != 0 }
+}
+
+#[inline]
+pub fn UpdateWindow(hWnd: HWND) -> Result<(), u32>
+{
+    let result = unsafe { user32::UpdateWindow(hWnd) };
 
     if result != 0 { Ok(()) }
     else { Err(GetLastError()) }
@@ -72,32 +125,133 @@ pub fn MessageBoxW(
 }
 
 #[inline]
-pub fn GetStdHandle(handle_id: u32) -> Result<Option<HANDLE>, u32>
+pub fn RegisterClassExW(wnd_cls: &WNDCLASSEXW)
+    -> Result<ATOM, u32>
 {
-    let handle = unsafe { kernel32::GetStdHandle(handle_id) };
+    let result = unsafe { user32::RegisterClassExW(wnd_cls as *const WNDCLASSEXW) };
+    if result != 0 { Ok(result) }
+    else { Err(GetLastError()) }
+}
 
-    if handle == 0 as HANDLE { Ok(None) }
-    else if handle != INVALID_HANDLE_VALUE { Ok(Some(handle)) }
+pub enum WinClsW<'a> {
+    Atom(ATOM),
+    ClsName(&'a [u16]),
+}
+
+#[inline]
+pub fn CreateWindowExW(
+    ex_style: u32,
+    cls_name: WinClsW,
+    window_name: Option<&[u16]>,
+    style: u32,
+    x: c_int,
+    y: c_int,
+    width: c_int,
+    height: c_int,
+    wnd_parent: Option<HWND>,
+    menu: Option<HMENU>,
+    instance: HINSTANCE,
+    param: Option<LPVOID>)
+    -> Result<HWND, u32>
+{
+    let result = unsafe {
+        let cls_ptr = match cls_name {
+            WinClsW::Atom(x) => x as LPCWSTR,
+            WinClsW::ClsName(n) => (*n).as_ptr() as LPCWSTR,
+        };
+
+        user32::CreateWindowExW(
+            ex_style, cls_ptr,
+            window_name.map_or(0 as LPCWSTR, |x|x.as_ptr() as LPCWSTR),
+            style, x, y, width, height,
+            wnd_parent.unwrap_or(0 as HWND),
+            menu.unwrap_or(0 as HMENU),
+            instance,
+            param.unwrap_or(0 as LPVOID))
+    };
+
+    if result != 0 as HWND { Ok(result) }
+    else { Err(GetLastError()) }
+}
+
+pub struct BlockingMsgLoop {
+    hwnd: Option<HWND>,
+}
+
+impl Iterator for BlockingMsgLoop {
+    type Item = Result<MSG, u32>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Result<MSG, u32>> {
+        let mut msg = MSG {
+            hwnd: 0 as HWND,
+            message: 0,
+            wParam: 0,
+            lParam: 0,
+            time: 0,
+            pt: POINT {x: 0, y: 0},
+        };
+        let hwnd = self.hwnd.unwrap_or(0 as HWND);
+        let result = unsafe { user32::GetMessageW(&mut msg as *mut MSG, hwnd, 0, 0) };
+
+        if result == 0 { None }
+        else if result == -1 { Some(Err(GetLastError())) }
+        else { Some(Ok(msg)) }
+    }
+}
+
+pub fn blocking_msg_loop(hwnd: Option<HWND>) -> BlockingMsgLoop
+{ BlockingMsgLoop { hwnd: hwnd } }
+
+pub struct PeekingMsgLoop {
+    hwnd: Option<HWND>,
+}
+
+impl Iterator for PeekingMsgLoop {
+    type Item = Result<MSG, u32>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Result<MSG, u32>> {
+        let mut msg = unsafe { std::mem::zeroed::<MSG>() };
+        let hwnd = self.hwnd.unwrap_or(0 as HWND);
+        let result = unsafe { user32::PeekMessageW(&mut msg as *mut MSG, hwnd, 0, 0, PM_REMOVE) };
+
+        if result == 0 { None }
+        else if result == -1 { Some(Err(GetLastError())) }
+        else { Some(Ok(msg)) }
+    }
+}
+
+pub fn peeking_msg_loop(hwnd: Option<HWND>) -> PeekingMsgLoop
+{ PeekingMsgLoop { hwnd: hwnd } }
+
+
+#[inline]
+pub fn DestroyWindow(hwnd: HWND) -> Result<(), u32>
+{
+    let result = unsafe { user32::DestroyWindow(hwnd) };
+
+    if result != 0 { Ok(()) }
     else { Err(GetLastError()) }
 }
 
 #[inline]
-pub fn WriteFile(
-    handle: HANDLE,
-    buffer: &[u8],
-    overlapped: Option<&mut OVERLAPPED>)
-    -> Result<u32, u32>
-{
-    //assert!(buffer.len() <= u32::max_value() as usize, "buffer length has to fit into u32");
-    let buffer_len = buffer.len() as u32;
-    let mut written: u32 = 0u32;
-    let result = unsafe {
-        let buffer_ptr = buffer.as_ptr() as *mut winapi::c_void;
-        let written_ptr = &mut written as *mut u32;
-        let overlapped_ptr = overlapped.map_or(0 as *mut OVERLAPPED, |x|x as *mut OVERLAPPED);
-        kernel32::WriteFile(handle, buffer_ptr, buffer_len, written_ptr, overlapped_ptr)
-    };
+pub fn DefWindowProcW(
+    hwnd: HWND,
+    msg: UINT,
+    param: WPARAM,
+    para: LPARAM)
+    -> LRESULT
+{ unsafe { user32::DefWindowProcW(hwnd, msg, param, para)} }
 
-    if result != 0 { Ok(written) }
-    else { Err(GetLastError()) }
-}
+#[inline]
+pub fn PostQuitMessage(exit_code: c_int)
+{ unsafe { user32::PostQuitMessage(exit_code)} }
+
+#[inline]
+pub fn TranslateMessage(msg: &MSG) -> bool
+{ unsafe { user32::TranslateMessage(msg as *const MSG) != 0 } }
+
+#[inline]
+pub fn DispatchMessageW(msg: &MSG) -> isize
+{ unsafe { user32::DispatchMessageW(msg as *const MSG) as isize } }
